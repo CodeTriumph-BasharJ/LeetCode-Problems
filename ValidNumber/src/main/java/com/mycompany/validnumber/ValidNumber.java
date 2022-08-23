1331c260cc8406e98f9378d80b9961cc18970b13
basha <basha@BasharPC>
2022-08-23 07:06:06 -0700

    Merge origin/master
    Conflicts:
    	Permutations/src/main/java/com/mycompany/permutations/Permutations.java

--- ValidNumber/src/main/java/com/mycompany/validnumber/ValidNumber.java
+++ ValidNumber/src/main/java/com/mycompany/validnumber/ValidNumber.java
@@ -15,16 +15,21 @@ public class ValidNumber {
     
     public static void main(String[] args) {
         boolean valid = true;
-        String number = "+987";
+        String number = "e987";
         String number_mod = number.toLowerCase();
         int dot = 0,e_ind = 0;
         
+        if(number_mod.contains(".") && number.indexOf(".") != number.lastIndexOf("."))valid=false;
+        
+        else if(valid == true){
         if(number_mod.contains("."))dot = number_mod.indexOf(".");
         else  dot = -1;
+        
         if(number_mod.indexOf("e")!=number_mod.lastIndexOf("e")) valid=false;
         if(number_mod.contains("e")) e_ind = number_mod.indexOf("e"); 
         if(number_mod.matches(".*[a-df-z].*")) valid = false;
         if(number_mod.matches(".*[?!#$%^&*=<>,'].*"))valid = false;
+        }  
         
         if (valid == true){
         for(int i = 0; i < number_mod.length(); ++i){
@@ -38,7 +43,7 @@ public class ValidNumber {
          } else if(String.valueOf(number_mod.charAt(i)).equals("e") && dot != -1 && number_mod.indexOf("e") <= dot + 1) {
              valid = false; 
              break;
-         } else if(String.valueOf(number_mod.charAt(i)).equals("//") ||String.valueOf(number_mod.charAt(i)).equals("[")
+         } else if(String.valueOf(number_mod.charAt(i)).equals("//") || String.valueOf(number_mod.charAt(i)).equals("[")
                  || String.valueOf(number_mod.charAt(i)).equals("]") || String.valueOf(number_mod.charAt(i)).equals("{")
                  || String.valueOf(number_mod.charAt(i)).equals("}") || String.valueOf(number_mod.charAt(i)).equals("(")
                  || String.valueOf(number_mod.charAt(i)).equals(")"))
